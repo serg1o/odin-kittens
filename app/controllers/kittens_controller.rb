6513cf0aa58580fd607ce999fc3a22ccd6cc3beb
Sérgio Ribeiro <sergiomsr@gmail.com>
2015-01-02 16:43:44 +0000

    basic kitten app with CRUD

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,53 @@
+class KittensController < ApplicationController
+
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    @kitten = Kitten.find(params[:id])
+    if @kitten.update_attributes(kitten_params)
+      flash[:success] = "Kitten updated!"
+      redirect_to root_url
+    else
+      
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Kitten.find(params[:id]).destroy
+    flash[:success] = "Kitten deleted"
+    redirect_to root_url
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash[:success] = "Kitten created!"
+      redirect_to root_url
+    else
+      
+      render 'new'
+    end
+  end
+
+  private
+
+  def kitten_params
+    params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+  end
+
+end
